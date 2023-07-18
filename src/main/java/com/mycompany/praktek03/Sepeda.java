e4e46795f1deea6599433558bc296a886c4aa68e
Ikynuji <rizky.new.appp@gmail.com>
2023-07-18 15:30:23 +0700

    Membuat atribut

--- src/main/java/com/mycompany/praktek03/Sepeda.java
+++ src/main/java/com/mycompany/praktek03/Sepeda.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.praktek03;
 
-/**
- *
- * @author User
- */
 public class Sepeda {
-    
+    String merk;
+    String warna;
+    double harga;
 }
